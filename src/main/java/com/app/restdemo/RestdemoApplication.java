5fe119dc89f9232de0bf337a3d9d6c6350cd21ce
Nehal Chamatkar <nexalphawerewolf@gmail.com>
2022-03-24 15:25:20 +0530

    feature and master merge

--- src/main/java/com/app/restdemo/RestdemoApplication.java
+++ src/main/java/com/app/restdemo/RestdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestdemoApplication {
 
 	public static void main(String[] args) {
+		//start spring boot application
 		SpringApplication.run(RestdemoApplication.class, args);
 		//master branch commit
 	}
